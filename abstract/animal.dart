226b520d7452ee4f69c75aad946c5f04a321b7d4
favjiw <mfavianj@gmail.com>
2025-12-16 11:35:35 +0700

    abstract

---
+++ abstract/animal.dart
@@ -0,0 +1,7 @@
+abstract class Animal {
+  String name;
+  int age;
+  double weight;
+
+  Animal(this.name, this.age, this.weight);
+}
---
+++ abstract/main.dart
@@ -0,0 +1,6 @@
+import 'animal.dart';
+
+void main(){
+  var dog = Animal('Zaki', 5, 20.0);
+  print('Dog Name: ${dog.name}, Age: ${dog.age}, Weight: ${dog.weight}');
+}
